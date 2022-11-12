89f450f3890c40844b8b674a161174f0d4e13887
kiyana kiyaee <kiyana76@gmail.com>
2022-11-12 15:51:32 +0330

    initial project and migration #wallet

---
+++ Wallet/app/Models/WalletTransaction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WalletTransaction extends Model
+{
+    use HasFactory;
+
+    const OPERATION_INCREASE = 'increase';
+    const OPERATION_DECREASE = 'decrease';
+    const OPERATION = [
+        self::OPERATION_INCREASE,
+        self::OPERATION_DECREASE
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'amount',
+        'operation',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+    ];
+}
---
+++ Wallet/database/migrations/2022_11_12_115254_create_wallet_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\WalletTransaction;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallet_transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('wallet_id')->unsigned();
+            $table->foreign('wallet_id')->references('id')->on('wallets')->onDelete('cascade');
+
+            $table->bigInteger('amount')->unsigned();
+            $table->enum('operation', WalletTransaction::OPERATION);
+
+            $table->morphs('agentable');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallet_transactions');
+    }
+}
