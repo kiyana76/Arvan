d06ab7554677251ef4ab842c3ba0ab82fde0fbdf
kiyana kiyaee <kiyana76@gmail.com>
2022-11-12 17:19:34 +0330

    add some config

--- Wallet/app/Http/Controllers/Controller.php
+++ Wallet/app/Http/Controllers/Controller.php
@@ -10,4 +10,28 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    // Success
+    const SUCCESS        = 20000;
+    const SUCCESS_VERIFY = 20001;
+    const SUCCESS_APPLY  = 20002;
+
+    protected function apiResponse($data, $code = 20000, $message = '')
+    {
+        if (!$message) {
+            $key     = "code_responses.{$code}";
+            $message = trans($key);
+
+            if ($key == $message) {
+                $message = '';
+            }
+        }
+
+        return [
+            'success' => $code >= 20000,
+            'message' => $message,
+            'code'    => $code,
+            'data'    => $data,
+        ];
+    }
 }
